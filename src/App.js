afa723b4e03e73ad6823a548d3aacc9c3f24b55d
Anvay Bhanap <anvay.bhanap@gmail.com>
2022-11-14 18:49:03 -0800

    main partitions made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
-import './App.css';
+import Intro from './components/intro/Intro';
+
 
 function App() {
   return (
     <div className="App">
-     Hello world
+      <Intro />
     </div>
   );
 }
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Intro.css'
+
+const Intro = () => {
+  return (
+    <div className="main">
+      
+        <div className="main-left">
+            <div className="main-left-wrapper">
+              <h2 className="main-intro">Hi, My name is</h2>
+              <h1 className="main-name">John Burton</h1>
+
+              <div className="main-title">
+                <div className="main-title-wrapper">
+                  <div className="main-title-item">Web Developer</div>
+                  <div className="main-title-item">UI/UX</div>
+                  <div className="main-title-item">Photographer</div>
+                  <div className="main-title-item">Writer</div>
+                  <div className="main-title-item">Content Creator</div>
+                </div>
+              </div>
+
+            </div>
+        </div>
+      <div className="main-right">right
+      
+      </div>
+    </div>
+  ) 
+}
+
+export default Intro
